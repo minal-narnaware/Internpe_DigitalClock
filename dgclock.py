c1d5fdf2f656742f5d172d450a3e589b7d13756f
minal narnaware <109660207+minal-narnaware@users.noreply.github.com>
2023-06-05 17:17:36 +0530

    Add files via upload

---
+++ dgclock.py
@@ -0,0 +1,25 @@
+import sys
+from tkinter import *
+import time
+
+def times():
+    current_time=time.strftime("%H:%M:%S")
+    clock.config(text=current_time)
+    clock.after(200,times)
+
+
+root  = Tk()
+root.geometry("400x250")
+clock=Label(root,font=("Arial",42,"bold"),bg="lightblue")
+clock.grid(row=2,column=2,pady=25,padx=100)
+times()
+
+digi = Label(root,text="Minal's Digital Clock",font="times 24 italic")
+digi.grid(row=0,column=2)
+
+
+nota=Label(root,text=" hours  minutes   seconds   ", font="times 15 bold")
+
+nota.grid(row=3,column=2)
+
+root.mainloop()
